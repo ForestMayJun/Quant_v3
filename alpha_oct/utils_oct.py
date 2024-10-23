7f2c681ac77a4362e6b04a1feccce3e3bd330a66
lvangge@shifengamc.com <2364996554@qq.com>
2024-10-23 14:13:05 +0800

    Oct 23th

--- alpha_oct/utils_oct.py
+++ alpha_oct/utils_oct.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 tqdm.pandas()
+import multiprocessing
+import matplotlib.pyplot as plt
 
 def split_ret_inday(p_inday:pd.DataFrame, time_split):
     '''
@@ -37,5 +39,58 @@ def inverse(price:pd.DataFrame, l_min=30):
     inv_per = sym.sum(axis=1) / num
     
     return inv_per, sym, num
+
+def multiporcessing_generate_factor(data):
+    '''
+    传入multiindex对象, level1-InstrumentID, level2-日频data, columns为日频价量数据, 可以多列
+    '''
+    shift_day = 0
+    r_win = 20
+    n_processing = 64
+
+    def _handle_stockly(data_stock: pd.DataFrame, r_win=r_win):
+        def _f(x: pd.Series):
+            pass
+
+        return data_stock['close'].rolling(r_win).apply(_f)
+
+    def process_group(group, r_win=15):
+        instrument_id, group_data = group
+        return (instrument_id, _handle_stockly(group_data, r_win))
+
+    def _price_vol_weight_corr_v1(data: pd.DataFrame, n_processes=n_processing):
+        grouped = list(data.groupby(level='InstrumentID'))
+        with multiprocessing.Pool(processes=n_processes) as pool:
+            results = list(tqdm(pool.imap(process_group, grouped), total=len(grouped)))
+
+        factor_list = []
+        for instrument_id, factor in results:
+            factor_df = pd.DataFrame(factor.values, index=factor.index.get_level_values('date'), columns=[factor.index[0][0]])
+            factor_list.append(factor_df)
+
+        factor = pd.concat(factor_list, axis=1)
+
+        return factor.T
     
+    return _price_vol_weight_corr_v1(data)
+
+
+def draw_price_vol_unique(data:pd.DataFrame):
+    '''
+    data:MultiIndex对象, 一列价格一列成交量数据
+    '''
+    p_unique = data['Close'].unique()
+    vol_unique = pd.Series(index=p_unique)
+    for p in vol_unique.index:
+        vol_unique.loc[p] = data['LastVolume'][data['Close'] == p].sum()
+
+    plt.scatter(p_unique, vol_unique)
         
+def drop_level1_index(data:pd.DataFrame):
+    res = data.copy()
+    res.index.names = ['a', 'InstrumentID', 'Date']
+    id = res.index.droplevel('a')
+    res.index = id
+    del data
+
+    return res.unstack(level='InstrumentID')
